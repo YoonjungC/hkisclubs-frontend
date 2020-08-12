c82d3f98d066be17c74ec82b8b85facb23323818
Yoonjung Choi <230579@hkis.edu.hk>
2020-08-12 22:52:09 +0800

    Changed http to https

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,8 @@
 // api routes
 
-export const API_URL = "http://api.hkisclubs.com";
+export const API_URL = "https://api.hkisclubs.com";
 //export const API_URL = process.env.NODE_ENV === 'development' ? "http://localhost:8000" : "https://nameless-fortress-20160.herokuapp.com";
-export const FRONTEND_URL = "http://hkisclubs.com";
+export const FRONTEND_URL = "https://www.hkisclubs.com";
 //export const FRONTEND_URL = process.env.NODE_ENV === 'development' ? "http://localhost:3000" : process.env.FRONTEND_URL;
 // export const ADMIN_URL = process.env.NODE_ENV === 'development' ? "http://localhost:5000" : process.env.ADMIN_URL;
-export const ADMIN_URL = "http://admin.hkisclubs.com";
+export const ADMIN_URL = "https://admin.hkisclubs.com";
