e4140861ed325524c7f85ef23f1127db784d84fb
Yoonjung Choi <230579@hkis.edu.hk>
2020-08-21 01:19:36 +0800

    urls

--- src/constants/api.js
+++ src/constants/api.js
@@ -4,10 +4,10 @@
 //export const FRONTEND_URL = process.env.NODE_ENV === 'development' ? "http://localhost:3000" : process.env.FRONTEND_URL;
 // export const ADMIN_URL = process.env.NODE_ENV === 'development' ? "http://localhost:5000" : process.env.ADMIN_URL;
 
-// export const FRONTEND_URL = "https://www.hkisclubs.com";
-// export const ADMIN_URL = "https://admin.hkisclubs.com";
-// export const API_URL = "https://api.hkisclubs.com";
+export const FRONTEND_URL = "https://www.hkisclubs.com";
+export const ADMIN_URL = "https://admin.hkisclubs.com";
+export const API_URL = "https://api.hkisclubs.com";
 
-export const FRONTEND_URL = "http://localhost:3000";
-export const API_URL = "http://localhost:8000";
-export const ADMIN_URL = "http://localhost:5000";
+// export const FRONTEND_URL = "http://localhost:3000";
+// export const API_URL = "http://localhost:8000";
+// export const ADMIN_URL = "http://localhost:5000";
